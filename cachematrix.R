49ee93d9e1b9bfd7cc0c5324479629cb878e2942
Anna Lenkiewicz <anna.lenkiewicz@hotmail.com>
2016-04-18 22:40:02 +0100

    Caching the Inverse of a Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,12 +1,18 @@
 ## The functions in this module are used to cache matrix and its inverse. 
-## They are especially useful if matrix is large and creating an inverse can be time consuming
+## They are especially useful if matrix is large and creating an inverse can be 
+## time consuming.
+
 ## Usage:
 ## 1. Create or load a non-empty matrix
-## 2. Pass the matrix as a parameter to makeCacheMatrix and store the result in another variable - it will be a list where the values will be cached.
-## 3. Pass the list variable as a parameter to cacheSolve and either retrieve the inverse matrix from cache, or, 
-##    if it does not exist, calculate it and store it in cache.
-## Note: Running cacheSolve with a 'normal' matrix will cause an error. The variable which is passed to this funciton needs to be a list created first 
-##       using makeCacheMatrix.
+## 2. Pass the matrix as a parameter to makeCacheMatrix and store the result in 
+##    another variable - it will be a list where the values will be cached.
+## 3. Pass the list variable as a parameter to cacheSolve and either retrieve 
+##    the inverse matrix from cache, or, if it does not exist, calculate it and 
+##    store it in cache.
+
+## Note: Running cacheSolve with a 'normal' matrix will cause an error. 
+##       The variable which is passed to this funciton needs to be a list 
+##       created first using makeCacheMatrix.
 
 
 ## Function: makeCacheMatrix
